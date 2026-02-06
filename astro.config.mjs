3793c4780b0f8a8749dedaa20c945ebccffbc762
JordyPirata <jordi77alva@gmail.com>
2026-02-05 23:14:46 -0600

    feature (deploy): GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ambrosiapay.com',
   integrations: [react()],
   build: {
     inlineStylesheets: "always",
