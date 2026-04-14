3b4187a81a56e6fe7691eacb010b35deadac9c73
Ivan Robles <irae45@gmail.com>
2026-04-14 16:20:49 -0600

    move releases from Download to src/lib.

---
+++ src/lib/releases/index.ts
@@ -0,0 +1,2 @@
+export { fetchLatestRelease, findAssetByPattern, stripVersionPrefix, buildVerifyCommand, parseDownloadUrls } from './releases';
+export type { ReleaseAsset, GithubRelease, DownloadUrls } from './releases';
